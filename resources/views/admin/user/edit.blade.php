f24c46e4e6aa63a14f1e2943b9d806a66136fade
mostakimul1994 <mostakimul.1994@gmail.com>
2020-09-17 16:13:24 +0600

    User Crude done

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin.master')
-@section('title','Create New Admin')
+@section('title','Edit Admin')
 @section('content')
 
 <div class="row">
 	<div class="col-sm-12">
 		<div class="page-header-title">
-			<h4 class="pull-left page-title">Create New Admin </h4>
+			<h4 class="pull-left page-title">Update Admin </h4>
 			<ol class="breadcrumb pull-right">
-				<li><a href="{{ route ('admin.dashboard') }}">Dasdboard</a></li>
-				<li class="active">Create</li>
+				<li><a href="{{ route ('user.index') }}">User List</a></li>
+				<li class="active">Edit</li>
 			</ol>
 			<div class="clearfix"></div>
 		</div>
@@ -20,11 +20,11 @@
 		<div class="panel panel-primary">
 			<div class="panel-heading"><h3 class="panel-title">Admin Form</h3></div>
 			<div class="panel-body">
-				<form class="form-horizontal" role="form" action="{{ route ('user.store') }}" method="post" enctype="multipart/form-data">
-					@csrf
-					@include('admin.user._form')
-
-					<button type="submit" class="btn btn-success waves-effect pull-right waves-light m-1-8">Create</button>
+				  <form action="{{ route('user.update',$user->id) }}" class="form-horizontal" method="post" enctype="multipart/form-data">
+                        @csrf
+                        @method('put')
+                        @include('admin.user._form')
+					<button type="submit" class="btn btn-success waves-effect pull-right waves-light m-1-8">Update</button>
 				</form>
 			</div> <!-- panel-body -->
 		</div> <!-- panel -->
